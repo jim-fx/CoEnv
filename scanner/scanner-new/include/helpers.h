e23a11700a1fd4085950e6991bcd646e8883a32d
Max Richter <max.richter@smail.th-koeln.de>
2022-01-22 02:35:20 +0100

    feat: add rfid and led

---
+++ scanner/scanner-new/include/helpers.h
@@ -0,0 +1,16 @@
+#include "opencv2/opencv.hpp"
+
+#include "helpers.c"
+using namespace cv;
+
+double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
+void fourPointTransform(Mat src, Mat &dst, std::vector<Point> pts);
+Point scalePointOrigin(cv::Point point, cv::Point origin, float scale);
+Point rotatePointOrigin(cv::Point point, cv::Point origin, float angle);
+void orderPoints(std::vector<cv::Point> inpts, std::vector<cv::Point> &ordered);
+
+double _distance(Point p1, Point p2);
+
+Scalar ScalarHSV2BGR(uchar H, uchar S, uchar V);
+bool compareAreas(std::vector<cv::Point> a, std::vector<cv::Point> b);
+bool compareDistance(std::pair<Point, Point> p1, std::pair<Point, Point> p2);
